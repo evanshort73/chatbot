d2e0aebd9c86e562fefda2dbca689e777f4ac314
Than Jianrungsang <than.jianrungsang@goodsearch.com>
2016-10-15 15:18:43 -0700

    add randomness to the response, but will need to make the code cleaner

--- bot.py
+++ bot.py
@@ -1,21 +1,37 @@
 import editdistance
 import itertools
+import random
 
 with open("dialog.txt") as f:
     dialog = list(line.rstrip("\n") for line in f)
 
 dialog = [(x, bool(dialog[i + 1])) for i, x in enumerate(dialog) if x]
 
+def randomly_select_response(responses, total_probability):
+    selected_prob = total_probability * random.random()
+    sum_prob = 0
+    for response in responses:
+        sum_prob += response[1]
+        if sum_prob >= selected_prob:
+            return response
+
+def generate_probability(score):
+    BASE = 10.0
+    return BASE ** -score
+
 user_input = "dummy"
 while user_input:
+    possible_responses = []
+    total_probability = 0
     user_input = raw_input('say something: ').lower()
     response = ""
     for i, (line, good) in enumerate(dialog):
         if good:
             score = editdistance.eval(user_input, line.lower())
-            if not response or score < best_score:
-                response, _ = dialog[i + 1]
-                best_score = score
+            prob  = generate_probability(score)
+            possible_responses.append((dialog[i+1][0], prob, score))
+            total_probability += prob
+    response, prob, score = randomly_select_response(possible_responses, total_probability)
     print 'bot response: ', response
-    print '( score: ', best_score, ')'
+    print '( score: ', score, ')'
     print '__________________________'
