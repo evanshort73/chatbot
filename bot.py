35878fc3dc49f91d60e2e7207e9553451efea3c1
Than Jianrungsang <than.jianrungsang@goodsearch.com>
2016-10-15 13:04:29 -0700

    add more readable texts

--- bot.py
+++ bot.py
@@ -5,12 +5,13 @@ with open("dialog.txt") as f:
 
 user_input = "dummy"
 while user_input:
-    user_input = raw_input().lower()
+    user_input = raw_input('say something: ').lower()
     response = ""
     for i, line in enumerate(dialog):
         score = editdistance.eval(user_input, line.lower())
         if not response or score < best_score:
             response = dialog[(i+1) % len(dialog)]
             best_score = score
-    print response
-    print best_score
+    print 'bot response: ', response
+    print '( score: ', best_score, ')'
+    print '__________________________'
