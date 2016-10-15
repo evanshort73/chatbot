f46de8926fe77a3a1bb5c65181c2ff18e8e3b8ab
Than Jianrungsang <than.jianrungsang@goodsearch.com>
2016-10-15 13:26:05 -0700

    separate dialog by conversation

--- movie_lines_extractor.py
+++ movie_lines_extractor.py
@@ -1,7 +1,19 @@
 import pdb
 
+DELIMETER = ' +++$+++ '
+dialog_hash = {}
+
+with open('movie_lines.txt') as f:
+    for line in f:
+        splitted_line = line.split(DELIMETER)
+        line_number = splitted_line[0].strip()
+        sentence = splitted_line[-1].strip()
+        dialog_hash[line_number] = sentence
+
 with open('dialog.txt', 'w+') as dialog_fd:
-    with open('movie_lines.txt') as f:
-        lines = f.readlines()
-        for line in lines:
-            dialog_fd.write(line.split('+++$+++')[-1].strip() + '\n')
+    with open('movie_conversations.txt') as f:
+        for line in f:
+            line_positions = eval(line.split(DELIMETER)[-1].strip())
+            for line_position in line_positions:
+                dialog_fd.write(dialog_hash[line_position] + '\n')
+            dialog_fd.write('\n')
